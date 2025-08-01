b3948f94142e3ba67f5491d7219b79f61aff1e78
Ailton Duarte <adidas.coyas@gmail.com>
2025-08-01 16:58:37 +0100

    feat: dashboard

---
+++ app/ui/dash/sidenav.tsx
@@ -0,0 +1,12 @@
+const SideBar = () => {
+	return (
+		<div className="flex flex-col h-full">
+			<div className="flex-1 overflow-y-auto p-4 bg-gray-300">
+				{/* Add your sidebar content here */}
+				<p>Dashboard SideBar Content</p>
+			</div>
+		</div>
+	);
+};
+
+export default SideBar;
