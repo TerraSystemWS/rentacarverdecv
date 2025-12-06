286313ad1f337d32d64892916c5034de2faa3988
Ailton Duarte <adidas.coyas@gmail.com>
2025-12-06 11:51:36 +0000

    fix: new wave

--- app/ui/front/minis/Header.tsx
+++ app/ui/front/minis/Header.tsx
@@ -1,61 +1,12 @@
 "use client";
-import React, { useEffect } from "react";
+
+import FSideBar from "@/app/ui/front/minis/FSidebar";
+import { useState } from "react";
 import Link from "next/link";
 
-const Header: React.FC = () => {
-	useEffect(() => {
-		// Este useEffect garante que o DOM está pronto antes de tentar anexar event listeners
-		const initHeaderModals = () => {
-			// Fechar search overlay
-			const searchCloseBtn = document.querySelector(
-				".overlay-scale .overlay-close"
-			);
-			if (searchCloseBtn) {
-				searchCloseBtn.addEventListener("click", () => {
-					const searchOverlay = document.querySelector(".overlay-scale");
-					searchOverlay?.classList.remove("open");
-				});
-			}
-
-			// Fechar sidebar overlay
-			const sidebarCloseBtn = document.querySelector(
-				".overlay-sidebar .closebtn"
-			);
-			if (sidebarCloseBtn) {
-				sidebarCloseBtn.addEventListener("click", () => {
-					const sidebarOverlay = document.querySelector(".overlay-sidebar");
-					sidebarOverlay?.classList.remove("open");
-				});
-			}
-
-			// Abrir search overlay
-			const searchOpenBtn = document.querySelector(".search-open");
-			if (searchOpenBtn) {
-				searchOpenBtn.addEventListener("click", (e) => {
-					e.preventDefault();
-					const searchOverlay = document.querySelector(".overlay-scale");
-					searchOverlay?.classList.add("open");
-				});
-			}
-
-			// Abrir sidebar overlay
-			const sidebarOpenBtn = document.querySelector(".trigger-overlay");
-			if (sidebarOpenBtn) {
-				sidebarOpenBtn.addEventListener("click", (e) => {
-					e.preventDefault();
-					const sidebarOverlay = document.querySelector(".overlay-sidebar");
-					sidebarOverlay?.classList.add("open");
-				});
-			}
-		};
-
-		initHeaderModals();
-
-		// Cleanup function
-		return () => {
-			// Remover event listeners se necessário
-		};
-	}, []);
+const Header = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [isPesquisaOpen, setIsPesquisaOpen] = useState(false);
 
 	return (
 		<>
@@ -67,19 +18,19 @@ const Header: React.FC = () => {
 							<div className="header-content-left">
 								<ul className="header-top-menu">
 									<li>
-										<Link href="tel:02385810945" className="top-left-menu">
+										<a href="tel:02385810945" className="top-left-menu">
 											<i className="fa fa-phone"></i>
 											<span>Chamar - 5810945</span>
-										</Link>
+										</a>
 									</li>
 									<li>
-										<Link
+										<a
 											href="mailto:reservas@rentacarverde.cv"
 											className="top-left-menu"
 										>
 											<i className="fa fa-envelope"></i>
 											<span>reservas@rentacarverde.cv</span>
-										</Link>
+										</a>
 									</li>
 								</ul>
 							</div>
@@ -89,36 +40,40 @@ const Header: React.FC = () => {
 							<div className="header-content-right">
 								<ul className="header-top-menu">
 									<li>
-										<Link href="#" className="language">
+										<a href="#" className="language">
 											<i className="fa fa-language"></i>
 											<span>Linguas</span>
-										</Link>
+										</a>
 										<ul className="sub-menu">
 											<li>
-												<Link href="#">English</Link>
+												<a href="#">English</a>
 											</li>
 											<li>
-												<Link href="#">Español</Link>
-											</li>
-											<li>
-												<Link href="#">Français</Link>
-											</li>
-											<li>
-												<Link href="#">Português</Link>
+												<a href="#">Português</a>
 											</li>
 										</ul>
 									</li>
 								</ul>
+
 								<ul className="header-top-menu">
 									<li>
-										<Link href="#" className="search-open">
+										<a
+											href="#"
+											className="search-open"
+											onClick={() => setIsPesquisaOpen(!isPesquisaOpen)}
+										>
 											<i className="fa fa-search"></i>
-										</Link>
+										</a>
 									</li>
+
 									<li>
-										<Link href="#" className="trigger-overlay">
+										<a
+											href="#"
+											className="trigger-overlay"
+											onClick={() => setIsOpen(!isOpen)}
+										>
 											<i className="fa fa-bars"></i>
-										</Link>
+										</a>
 									</li>
 								</ul>
 							</div>
@@ -127,13 +82,20 @@ const Header: React.FC = () => {
 				</div>
 			</header>
 
-			{/* ======= Header Modal Area =======*/}
+			{/* ===== Header Modal====== */}
 			<div className="header-modal-area">
 				{/* Modal Search */}
-				<div className="overlay overlay-scale">
-					<button type="button" className="overlay-close">
+				<div
+					className={`overlay overlay-scale ${isPesquisaOpen ? "open" : ""}`}
+				>
+					<button
+						type="button"
+						className="overlay-close"
+						onClick={() => setIsPesquisaOpen(false)}
+					>
 						&#x2716;
 					</button>
+
 					<div className="overlay__content">
 						<form
 							id="search-form"
@@ -148,6 +110,7 @@ const Header: React.FC = () => {
 									placeholder="search text here ..."
 								/>
 							</div>
+
 							<button className="btn text-uppercase search-button">
 								Pesquisar
 							</button>
@@ -155,73 +118,8 @@ const Header: React.FC = () => {
 					</div>
 				</div>
 
-				<div className="overlay-sidebar">
-					<div className="author-area">
-						<Link href="#" className="closebtn">
-							&times;
-						</Link>
-						<div className="author-area-content">
-							<div className="login-author">
-								<div className="author-info">
-									<div className="author-image yellow-border">
-										<img
-											src="/assets/images/driver/driver-03.png"
-											alt="author-image"
-										/>
-									</div>
-									<div className="author-des">
-										<h4 className="author-name">Mr. Johan Smith</h4>
-										<p className="author-description">Programmer</p>
-									</div>
-								</div>
-								<div className="author-menu">
-									<ul className="yellow-color">
-										<li>
-											<Link href="">
-												<i className="fa fa-user-circle-o"></i>Author Dashboard
-											</Link>
-										</li>
-										<li>
-											<Link href="">
-												<i className="fa fa-envelope-open"></i>Your Inbox
-											</Link>
-										</li>
-										<li>
-											<Link href="">
-												<i className="fa fa-location-arrow"></i>Track your texi
-											</Link>
-										</li>
-										<li>
-											<Link href="">
-												<i className="fa fa-area-chart"></i>Your Bookings Status
-											</Link>
-										</li>
-										<li>
-											<Link href="">
-												<i className="fa fa-automobile"></i>New Bookings
-											</Link>
-										</li>
-										<li>
-											<Link href="">
-												<i className="fa fa-archive"></i>Your Bookings
-											</Link>
-										</li>
-										<li>
-											<Link href="">
-												<i className="fa fa-money"></i>Your Deposit - $150.00
-											</Link>
-										</li>
-										<li>
-											<Link href="">
-												<i className="fa fa-sign-out"></i>Sign Out
-											</Link>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
+				{/* Sidebar */}
+				<FSideBar onToggleSidebar={() => setIsOpen(!isOpen)} isOpen={isOpen} />
 			</div>
 
 			{/* ====== Header Nav Area ====== */}
@@ -235,6 +133,7 @@ const Header: React.FC = () => {
 								</Link>
 							</div>
 						</div>
+
 						<div className="col-md-9 col-sm-2 col-xs-2 pd-right-0">
 							<nav className="site-navigation top-navigation nav-style-one">
 								<div className="menu-wrapper">
@@ -242,85 +141,15 @@ const Header: React.FC = () => {
 										<ul className="menu-list">
 											<li>
 												<Link href="/">Home</Link>
-												{/* <ul className="sub-menu">
-													<li>
-														<a href="index.html">Home Layout One</a>
-													</li>
-													<li>
-														<a href="index02.html">Home Layout Two</a>
-													</li>
-													<li>
-														<a href="index03.html">Home Layout Three</a>
-													</li>
-													<li>
-														<a href="index04.html">Home Layout Four</a>
-													</li>
-													<li>
-														<a href="index05.html">Home Layout Five</a>
-													</li>
-													<li>
-														<a href="index06.html">Home Layout Six</a>
-													</li>
-													<li>
-														<a href="index07.html">Home Layout Seven</a>
-													</li>
-													<li>
-														<a href="index08.html">Home Layout Eight</a>
-													</li>
-													<li>
-														<a href="index09.html">Home Layout Nine</a>
-													</li>
-													<li>
-														<a href="index10.html">Home Layout Ten</a>
-													</li>
-												</ul> */}
 											</li>
-											{/* <li>
-												<a href="/booking">Reservar</a>
-											</li> */}
 											<li>
 												<Link href="/cars">Viaturas</Link>
-												{/* <ul className="sub-menu">
-													<li>
-														<a href="car-category.html">Car Category 01</a>
-													</li>
-													<li>
-														<a href="car-category-two.html">Car Category 02</a>
-													</li>
-													<li>
-														<a href="car-category-three.html">
-															Car Category 03
-														</a>
-													</li>
-													<li>
-														<a href="car-single-page.html">Car Single Page</a>
-													</li>
-												</ul> */}
 											</li>
-											{/* <li>
-												<a href="#">Pages</a>
-												<ul className="sub-menu">
-													<li>
-														<a href="comming-soon.html">Coming Soon</a>
-													</li>
-													<li>
-														<a href="404.html">404</a>
-													</li>
-												</ul>
-											</li> */}
 											<li>
 												<Link href="/about">Sobre Nós</Link>
 											</li>
 											<li>
 												<Link href="/posts">Novidades</Link>
-												{/* <ul className="sub-menu">
-													<li>
-														<a href="blog.html">Blog</a>
-													</li>
-													<li>
-														<a href="blog-single.html">Single Post</a>
-													</li>
-												</ul> */}
 											</li>
 											<li>
 												<Link href="/contact">Contato</Link>
@@ -330,17 +159,17 @@ const Header: React.FC = () => {
 								</div>
 							</nav>
 
-							{/* Mobile Main Menu */}
+							{/* Mobile */}
 							<div className="mobile-menu-main hidden-md hidden-lg">
-								<div className="menucontent overlaybg"> </div>
+								<div className="menucontent overlaybg"></div>
 								<div className="menuexpandermain slideRight">
-									<Link
-										href={""}
+									<a
+										href=""
 										id="navtoggole-main"
 										className="animated-arrow slideLeft menuclose"
 									>
 										<span></span>
-									</Link>
+									</a>
 								</div>
 
 								<div id="mobile-main-nav" className="mb-navigation slideLeft">
